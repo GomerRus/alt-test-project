32521f9d8adaeea5ce097423b9d8a3e35c0eeea5
Gomer <alekseev.m.a.rus@gmail.com>
2026-02-12 13:43:24 +0300

    Split project into library and CLI utility modules

--- cli-app/src/main/java/ru/alt/linux/cli/CliApplication.java
+++ cli-app/src/main/java/ru/alt/linux/cli/CliApplication.java
@@ -1,7 +1,6 @@
-package ru.alt.linux;
+package ru.alt.linux.cli;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.shell.command.annotation.CommandScan;
 
